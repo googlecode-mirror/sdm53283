5e3e089cc8738a4d88b3cff7e68a433d188cd106
sofeeyah0702 <sofeeyah0702@gmail.com>
2013-09-10 06:54:08 +0000

    edit a bit of code ...classMainClass line 5

--- MyProjectSDM/src/MainClass.java
+++ MyProjectSDM/src/MainClass.java
@@ -2,7 +2,7 @@
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("How are you?");
+		System.out.println("How do you do?");
 	}
 
 }
